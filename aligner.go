4c07d44590957506716c3e6b805439ff703f9e5e
McMenemy <mr_mcmenemy@yahoo.com>
2016-11-16 10:18:28 -0800

    finish simple sequence align

--- aligner.go
+++ aligner.go
@@ -3,3 +3,107 @@ package aligner
 func Tester(str string) string {
 	return str
 }
+
+// Utility function and structs
+func initDpTable(seq1, seq2 string) [][]int {
+	dpTable := make([][]int, len(seq1))
+
+	// make rows
+	for i := range dpTable {
+		dpTable[i] = make([]int, len(seq2))
+	}
+
+	// add initial values
+	dpTable[0][0] = 0
+	for i := 1; i < len(seq1); i++ {
+		dpTable[i][0] = -2 * i
+	}
+	for j := 1; j < len(seq2); j++ {
+		dpTable[0][j] = -2 * j
+	}
+
+	return dpTable
+}
+
+type node struct {
+	score int
+	path  string
+}
+
+func Align(seq1, seq2 string) []string {
+	const gap = -2
+	const match = 1
+	const mismatch = -1
+
+	rowCount := len(seq1) + 1
+	colCount := len(seq2) + 1
+	dpTable := make([][]node, rowCount)
+
+	// make rows
+	for i := range dpTable {
+		dpTable[i] = make([]node, colCount)
+	}
+
+	// add initial values
+	dpTable[0][0] = node{0, "start"}
+	for i := 1; i < rowCount; i++ {
+		dpTable[i][0] = node{gap * i, "up"}
+	}
+	for j := 1; j < colCount; j++ {
+		dpTable[0][j] = node{gap * j, "right"}
+	}
+
+	// fill in scores
+	for i := 1; i < rowCount; i++ {
+		for j := 1; j < colCount; j++ {
+			var pairScore int
+			if seq1[i-1] == seq2[j-1] {
+				pairScore = match
+			} else {
+				pairScore = mismatch
+			}
+
+			rightScore := dpTable[i][j-1].score + gap
+			upScore := dpTable[i-1][j].score + gap
+			diagScore := dpTable[i-1][j-1].score + pairScore
+
+			if rightScore >= upScore && rightScore >= diagScore {
+				dpTable[i][j] = node{rightScore, "right"}
+			} else if upScore >= rightScore && upScore >= diagScore {
+				dpTable[i][j] = node{upScore, "up"}
+			} else if diagScore >= rightScore && diagScore >= upScore {
+				dpTable[i][j] = node{diagScore, "diag"}
+			}
+		}
+	}
+
+	// use dp table to backtrace sequence pairings
+	var result []string
+
+	i := len(seq1) - 1
+	j := len(seq2) - 1
+	done := false
+	for !done {
+		currentNode := dpTable[i+1][j+1]
+		if currentNode.path == "up" {
+			result = append(result, string(seq1[i])+"_")
+			i -= 1
+		} else if currentNode.path == "right" {
+			result = append(result, "_"+string(seq2[j]))
+			j -= 1
+		} else if currentNode.path == "diag" {
+			result = append(result, string(seq1[i])+string(seq2[j]))
+			i -= 1
+			j -= 1
+		} else if currentNode.path == "start" {
+			done = true
+		}
+	}
+
+	// reverse the result array for correct answer
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+
+	return result
+}
--- aligner_test.go
+++ aligner_test.go
@@ -1,13 +1,70 @@
 package aligner
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestTester(t *testing.T) {
+	fmt.Println("canary test")
 	actual := Tester("abc")
-	expected := "t push --set-upstream mcm masterabc"
+	expected := "abc"
 
 	if actual != expected {
 		t.Error("Tester function failed expected output")
 		t.Logf("expected %s but got %s", expected, actual)
+	}
+}
+
+// Utility functions and structs
+type AlignTest struct {
+	seq1, seq2, message string
+	expected            []string
+}
+
+func compareArrays(arr1, arr2 []string) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	for i, element1 := range arr1 {
+		element2 := arr2[i]
+		if element1 != element2 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Tests
+func TestAlign(t *testing.T) {
+	alignTests := []AlignTest{
+		AlignTest{
+			"gcat",
+			"gcat",
+			"Align does not work with identical sequences",
+			[]string{"gg", "cc", "aa", "tt"},
+		},
+		AlignTest{
+			"gt",
+			"ca",
+			"Align does not work when there are no matches",
+			[]string{"gc", "ta"},
+		},
+		AlignTest{
+			"actag",
+			"ctaga",
+			"Align does not work with gaps",
+			[]string{"a_", "cc", "tt", "aa", "gg", "_a"},
+		},
+	}
+
+	for _, test := range alignTests {
+		actual := Align(test.seq1, test.seq2)
+		if !compareArrays(actual, test.expected) {
+			t.Error(test.message)
+			t.Logf("expected %v, actual %v", test.expected, actual)
+		}
 	}
 }
