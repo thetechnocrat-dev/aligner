56e6202564f7bf5aa317e7e307bea5164ffe4490
McMenemy <mr_mcmenemy@yahoo.com>
2017-01-20 22:14:01 -0800

    change to affline alignment

--- aligner.go
+++ aligner.go
@@ -1,5 +1,9 @@
 package aligner
 
+import (
+	"math"
+)
+
 func Tester(str string) string {
 	return str
 }
@@ -14,20 +18,30 @@ func Align(seq1, seq2 string, matchScore, mismatchPenalty, gapPenalty,
 
 	rowCount := len(seq1) + 1
 	colCount := len(seq2) + 1
-	dpTable := make([][]node, rowCount)
+	xTable := make([][]node, rowCount)
+	yTable := make([][]node, rowCount)
+	mTable := make([][]node, rowCount)
 
 	// make rows
-	for i := range dpTable {
-		dpTable[i] = make([]node, colCount)
+	for i := range xTable {
+		xTable[i] = make([]node, colCount)
+		yTable[i] = make([]node, colCount)
+		mTable[i] = make([]node, colCount)
 	}
 
 	// add initial values
-	dpTable[0][0] = node{0, "start"}
+	xTable[0][0] = node{0, "start"}
+	yTable[0][0] = node{0, "start"}
+	mTable[0][0] = node{0, "start"}
 	for i := 1; i < rowCount; i++ {
-		dpTable[i][0] = node{gapPenalty * float64(i), "up"}
+		xTable[i][0] = node{math.Inf(-1), "yx"}
+		yTable[i][0] = node{gapOpeningPenalty + (gapPenalty * float64(i)), "yy"}
+		mTable[i][0] = node{math.Inf(-1), "ym"}
 	}
 	for j := 1; j < colCount; j++ {
-		dpTable[0][j] = node{gapPenalty * float64(j), "right"}
+		xTable[0][j] = node{gapOpeningPenalty + (gapPenalty * float64(j)), "xx"}
+		yTable[0][j] = node{math.Inf(-1), "xy"}
+		mTable[0][j] = node{math.Inf(-1), "xm"}
 	}
 
 	// fill in Scores
@@ -40,38 +54,94 @@ func Align(seq1, seq2 string, matchScore, mismatchPenalty, gapPenalty,
 				pairScore = mismatchPenalty
 			}
 
-			rightScore := dpTable[i][j-1].Score + gapPenalty
-			upScore := dpTable[i-1][j].Score + gapPenalty
-			diagScore := dpTable[i-1][j-1].Score + pairScore
+			xx := xTable[i][j-1].Score + gapPenalty
+			yx := yTable[i][j-1].Score + gapOpeningPenalty + gapPenalty
+			mx := mTable[i][j-1].Score + gapOpeningPenalty + gapPenalty
+			if xx >= yx && xx >= mx {
+				xTable[i][j] = node{xx, "xx"}
+			} else if yx >= mx {
+				xTable[i][j] = node{yx, "yx"}
+			} else {
+				xTable[i][j] = node{mx, "mx"}
+			}
 
-			if rightScore >= upScore && rightScore >= diagScore {
-				dpTable[i][j] = node{rightScore, "right"}
-			} else if upScore >= rightScore && upScore >= diagScore {
-				dpTable[i][j] = node{upScore, "up"}
-			} else if diagScore >= rightScore && diagScore >= upScore {
-				dpTable[i][j] = node{diagScore, "diag"}
+			yy := yTable[i-1][j].Score + gapPenalty
+			xy := xTable[i-1][j].Score + gapOpeningPenalty + gapPenalty
+			my := mTable[i-1][i].Score + gapOpeningPenalty + gapPenalty
+			if yy >= xy && yy >= my {
+				yTable[i][j] = node{yy, "yy"}
+			} else if xy >= my {
+				yTable[i][j] = node{xy, "xy"}
+			} else {
+				yTable[i][j] = node{my, "my"}
+			}
+
+			mm := mTable[i-1][j-1].Score + pairScore
+			xm := xTable[i-1][j-1].Score + pairScore
+			ym := yTable[i-1][j-1].Score + pairScore
+			if mm >= xm && mm >= ym {
+				mTable[i][j] = node{mm, "mm"}
+			} else if xm >= ym {
+				mTable[i][j] = node{xm, "xm"}
+			} else {
+				mTable[i][j] = node{ym, "ym"}
 			}
 		}
 	}
 
 	// use dp table to backtrace sequence pairings
 	var result []string
-
-	i := len(seq1) - 1
-	j := len(seq2) - 1
+	i := len(seq1)
+	j := len(seq2)
+	var currentNode node
+	if xTable[i][j].Score >= mTable[i][j].Score && xTable[i][j].Score >= yTable[i][j].Score {
+		currentNode = xTable[i][j]
+	} else if yTable[i][j].Score >= mTable[i][j].Score {
+		currentNode = yTable[i][j]
+	} else {
+		currentNode = mTable[i][j]
+	}
 	done := false
 	for !done {
-		currentNode := dpTable[i+1][j+1]
-		if currentNode.Path == "up" {
-			result = append(result, string(seq1[i])+"_")
-			i -= 1
-		} else if currentNode.Path == "right" {
-			result = append(result, "_"+string(seq2[j]))
+		if currentNode.Path == "xx" {
+			result = append(result, "_"+string(seq2[j-1]))
 			j -= 1
-		} else if currentNode.Path == "diag" {
-			result = append(result, string(seq1[i])+string(seq2[j]))
+			currentNode = xTable[i][j]
+		} else if currentNode.Path == "yx" {
+			result = append(result, "_"+string(seq2[j-1]))
+			j -= 1
+			currentNode = yTable[i][j]
+		} else if currentNode.Path == "mx" {
+			result = append(result, "_"+string(seq2[j-1]))
+			j -= 1
+			currentNode = mTable[i][j]
+		} else if currentNode.Path == "yy" {
+			result = append(result, string(seq1[i-1])+"_")
+			i -= 1
+			currentNode = yTable[i][j]
+		} else if currentNode.Path == "xy" {
+			result = append(result, string(seq1[i-1])+"_")
+			i -= 1
+			currentNode = xTable[i][j]
+		} else if currentNode.Path == "my" {
+			result = append(result, string(seq1[i-1])+"_")
+			i -= 1
+			currentNode = mTable[i][j]
+		} else if currentNode.Path == "mm" {
+			result = append(result, string(seq1[i-1])+string(seq2[j-1]))
 			i -= 1
 			j -= 1
+			currentNode = mTable[i][j]
+		} else if currentNode.Path == "xm" {
+			result = append(result, string(seq1[i-1])+string(seq2[j-1]))
+			i -= 1
+			j -= 1
+			currentNode = xTable[i][j]
+		} else if currentNode.Path == "ym" {
+			result = append(result, string(seq1[i-1])+string(seq2[j-1]))
+			i -= 1
+			j -= 1
+			currentNode = yTable[i][j]
 		} else if currentNode.Path == "start" {
 			done = true
 		}
--- aligner_test.go
+++ aligner_test.go
@@ -37,9 +37,9 @@ func compareArrays(arr1, arr2 []string) bool {
 
 // Tests
 func TestAlign(t *testing.T) {
-	const matchScore = float64(1)
-	const mismatchPenalty = float64(-1)
-	const gapPenalty = float64(-1)
+	const matchScore = float64(2)
+	const mismatchPenalty = float64(-6)
+	const gapPenalty = float64(-4)
 	const gapOpeningPenalty = float64(-4)
 
 	alignTests := []AlignTest{
